6fa94d084647cbefb22839aa9e295685dca255a0
frau0006 <frau0006@algonquinlive.com>
2022-02-28 18:25:49 -0500

    Update infix_to_postfix.py
    Added power operator, order of precedence and multiplication injection to support multiplying against brackets.

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -5,6 +5,10 @@
 # This will convert an expression written in infix notation into postfix
 # (or Reverse Polish) notation, and then evaluate it.
 
+# A notable shortcoming is the lack of support for numbers with decimal
+# points! (You could still use fractional numbers i.e. instead of 0.25
+# you could enter (1/4)... )
+
 import sys
 import re
 import operator
@@ -22,21 +26,39 @@ def split_into_terms(expr: str) -> list:
     #       () to include the delimiting characters
     #       [] matches everything insidex
     # filter None since the regex produces some empty elements
-    return list(filter(None, (re.split("([\+\-\*\/()])", expr) )))
+    return list(filter(None, (re.split("([\+\-\*\/\^()])", expr) )))
+
+
+# If there is a number against an open bracket, i.e. 2(3), it will inject
+# a '*' between them for easier parsing later. 2(3) -> 2*(3)
+def inject_multiplication(terms: list) -> list:
+    for i, c in enumerate(terms):
+        if c == '(':
+            if i > 0 and terms[i-1].isnumeric():
+                terms.insert(i, '*')
+    return terms
 
 
 def infix_to_postfix(terms: list) -> list:
     out_stack = []
     oper_stack = []
 
-    for i in split_into_terms(expr):
+    precedence = {
+        '+' : 1, '-' : 1,
+        '*' : 2, '/' : 2,
+        '^' : 3
+    }
+
+    for i in inject_multiplication(split_into_terms(expr)):
 
         if i.isnumeric():
             out_stack.append(i)
             continue
 
-        elif i in "+-*/":
-            if len(oper_stack) > 0 and oper_stack[len(oper_stack)-1] != '(' and i in "+-":
+        elif i in "+-*/^":
+            if len(oper_stack) > 0:
+                top = oper_stack[-1]
+            if len(oper_stack) > 0 and top != '(' and precedence[top] >= precedence[i]:
                 out_stack.append(oper_stack.pop())
             oper_stack.append(i)
         
@@ -65,17 +87,18 @@ def evaluate_postfix_expression(expr: list) -> float:
 
     ops = {
         '+' : operator.add,     '-' : operator.sub,
-        '*' : operator.mul,     '/' : operator.truediv
+        '*' : operator.mul,     '/' : operator.truediv,
+        '^' : operator.pow
     }
 
     for i in expr:
         if i.isnumeric():
-            stack.append(float(i))
+            stack.append(int(i))
             continue
 
-        elif i in "+-*/":
-            right_operand = float(stack.pop())
-            left_operand = float(stack.pop())
+        elif i in "+-*/^":
+            right_operand = stack.pop()
+            left_operand = stack.pop()
             stack.append(ops[i](left_operand, right_operand))
                 
         else:
